2fa0cebac71db3b974d7f0f262806a005ae58fea
jumper69 <mikosxdp@gmail.com>
2024-05-11 11:39:16 +0200

    Added About, Contact, PrivacyPolicy Pages in Views, updated and added new database used for project, added Admin area

---
+++ BlogWebApp/Controllers/PageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApp.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult PrivacyPolicy() 
+        {
+            return View();
+        }
+    }
+}
--- BlogWebApp/Program.cs
+++ BlogWebApp/Program.cs
@@ -44,7 +44,7 @@ namespace BlogWebApp
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
 
