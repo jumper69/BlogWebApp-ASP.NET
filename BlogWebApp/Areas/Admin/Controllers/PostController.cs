014edfdd79366e0d83c89021a3f0803118e0114c
jumper69 <mikosxdp@gmail.com>
2024-07-06 01:12:48 +0200

    Added list of posts in admin panel, added functionality of deleting posts

--- BlogWebApp/Areas/Admin/Controllers/PostController.cs
+++ BlogWebApp/Areas/Admin/Controllers/PostController.cs
@@ -30,9 +30,30 @@ namespace BlogWebApp.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var listOfPosts = new List<Post>();
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin)
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).ToListAsync();
+            }
+            else
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).Where(x => x.ApplicationUser!.Id == loggedInUser!.Id).ToListAsync();
+            }
+
+            var listOfPostsVM = listOfPosts.Select(x => new PostVM()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                CreatedDate = x.CreatedDate,
+                ThumbnailUrl = x.ThumbnailUrl,
+                AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser.LastName
+            }).ToList();
+            return View(listOfPostsVM);
         }
 
         [HttpGet]
@@ -71,6 +92,24 @@ namespace BlogWebApp.Areas.Admin.Controllers
             await _context.SaveChangesAsync();
             _notification.Success("Post Created Successfully");
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin || loggedInUser?.Id == post?.ApplicationUserId)
+            {
+                _context.Posts!.Remove(post!);
+                await _context.SaveChangesAsync();
+                _notification.Success("Post Deleted");
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+            return View();
         }
 
         private string UploadImage(IFormFile file)
