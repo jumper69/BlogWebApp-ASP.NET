f91d721f1f45179c32f10142c00695d332b1a23c
jumper69 <mikosxdp@gmail.com>
2024-07-06 00:32:38 +0200

    Added functionaliy of creating a post; title, short desc, description, image attachment

--- BlogWebApp/Areas/Admin/Controllers/PostController.cs
+++ BlogWebApp/Areas/Admin/Controllers/PostController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using BlogWebApp.Models;
 using BlogWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using BlogWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlogWebApp.Utilities;
 
 namespace BlogWebApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class PostController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostController(ApplicationDbContext context,
+                                INotyfService notyfService,
+                                IWebHostEnvironment webHostEnvironment,
+                                UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +39,51 @@ namespace BlogWebApp.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View(new CreatePostVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            
+            var post = new Post();
+           
+            post.Title = vm.Title;
+            post.Description = vm.Description;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+
+            if (post.Title != null)
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            }
+
+            if (vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Post Created Successfully");
+            return RedirectToAction("Index");
+        }
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
     }
 }
--- BlogWebApp/ViewModels/CreatePostVM.cs
+++ BlogWebApp/ViewModels/CreatePostVM.cs
@@ -1,10 +1,12 @@
 ﻿using BlogWebApp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogWebApp.ViewModels
 {
     public class CreatePostVM
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
         public string? ApplicationUserId { get; set; }
