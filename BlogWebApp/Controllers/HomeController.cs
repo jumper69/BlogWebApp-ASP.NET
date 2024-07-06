89f584cfed46b9fa00da0707c568fe623c6d26e8
jumper69 <mikosxdp@gmail.com>
2024-07-06 04:25:30 +0200

    home page design changes, post previews, login theme photo changed

--- BlogWebApp/Controllers/HomeController.cs
+++ BlogWebApp/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using BlogWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using BlogWebApp.ViewModels;
+using BlogWebApp.Data;
 
 namespace BlogWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM();
+            var setting = _context.Settings!.ToList();
+            vm.Title = setting[0].Title;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts = _context.Posts!.Include(x => x.ApplicationUser).ToList();
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ BlogWebApp/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using BlogWebApp.Models;
+
+namespace BlogWebApp.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
